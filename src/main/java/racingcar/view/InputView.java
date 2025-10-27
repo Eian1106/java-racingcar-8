ad5527e634b699bde6dcbc6ba21eecf9a9e5685c
ChoSeongHyeon <00seong116@naver.com>
2025-10-27 18:18:23 +0900

    refactor(validator): InputView 클래스 메소드명 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
     private static final String CARS_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String PLAY_ROUND_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
 
-    public String getCarsName() {
+    public String getCarNames() {
         System.out.println(CARS_NAME_INPUT_MESSAGE);
         return getUserInput();
     }
